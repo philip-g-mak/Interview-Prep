872d72aabbf53108dd738c09d424615e19dff039
Philip mak <philip.g.mak@gmail.com>
2021-03-02 20:04:40 -0500

    Time: 2 ms (95.95%), Space: 38.8 MB (67.16%) - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
+        
+        int roomCount = 0;
+        for(int startIndex = 0, endIndex = 0; startIndex < start.length; startIndex++){
+            if(start[startIndex] < end[endIndex]){
+                roomCount++;
+            } else {
+                endIndex++;
+            }
+        }
+        
+        return roomCount;
+        
+    }
+}
+
+/*
+idea - if there is overlap, we need another room?
+
+split into 2 arrays, start and end time and sort both ascending
+
+if a meeting starts before another meeting ends - increment
+else no conflict so keep comparing 
+*/
