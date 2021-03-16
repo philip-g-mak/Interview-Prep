f1768f681fe8807899d53a72d9265dfe0c109b3d
Philip mak <philip.g.mak@gmail.com>
2021-03-15 21:52:06 -0400

    Time: 4 ms (100.00%), Space: 41.1 MB (66.64%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,13 +9,11 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == p || root == q || root == null)
-            return root;
+        if(root == null || root == p || root == q) return root;
         
-        TreeNode leftLCA = lowestCommonAncestor(root.left, p ,q);
-        TreeNode rightLCA = lowestCommonAncestor(root.right, p ,q);
+        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
         
-        //if neighther left lca or right lca are null, then the root is the lca
         if(leftLCA != null && rightLCA != null){
             return root;
         }
@@ -25,9 +23,14 @@ class Solution {
 }
 
 /*
-idea - 
-bottom up 
+idea - search left and right subtrees
 
-find each node p and q
-then send the nodes upward until common one is reached 
+if the current (sub)tree contains both p and q, then lca is the root.  if only one lca is found, we know the other is not found in the other subtree therefore the lca is the p or q element we found
+
+
+either will be subtree-root node - or node p or q itself
+reasoning, if we leftcla and right cla are both found, the rootnode is the lowest ancestor by the fact that its the root of the tree
+
+if only one is found, that means we exhaused searched one subtree (left or right) and it was not there.  thereforce it must be beneath the the lca we did find.  
+so the lca is the p or q node we found.
 */
