8158483872266c9e1718d7d7b0fec4ec04a008b9
Philip mak <philip.g.mak@gmail.com>
2021-04-02 12:35:14 -0400

    Time: 13 ms (27.18%), Space: 39.4 MB (22.36%) - LeetHub

--- integer-to-english-words/integer-to-english-words.java
+++ integer-to-english-words/integer-to-english-words.java
@@ -1,35 +1,46 @@
 class Solution {
-    
-    String[] lessThan20 = {"", "One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen",};
+    String[] lessThan20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     
     String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    String[] thousand = {"", "Thousand", "Million", "Billion"};
+    
+    String[] thousands = {"", "Thousand", "Million", "Billion"};
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
         
-        String words = "";
-        int i = 0;
         
+        int i = 0;
+        String words="";
         while(num > 0){
-            if(num%1000 != 0){
-                words = helper(num%1000) + thousand[i] + " " + words;
+            if(num %1000 != 0){
+                words = helper(num%1000) +  thousands[i] + " " + words;
             }
-            i++;
+            
             num/=1000;
+            i++;
         }
         
-        
         return words.trim();
-        
     }
     
     public String helper(int num){
         if(num == 0) return "";
         
-        if(num <20) return lessThan20[num] + " ";
+        if(num < 20) return lessThan20[num] + " ";
         
-        if(num < 100) return tens[num/10] + " " +  helper(num%10);
+        if(num < 100) return tens[num/10] + " " + helper(num%10);
         
-        return lessThan20[num/100] + " Hundred " + helper(num%100);
+        if(num<1000) return lessThan20[num/100] + " Hundred " + helper(num%100);
+        
+        return "";
     }
 }
+
+/*
+break it into parts and call recursively
+
+thousand - million - billion
+
+call helper method on the num%1000 
+ to get the hundred tens ones
+
+*/
