b4e2e59bc2b41b6cd74b7208537f85fce632733e
Philip mak <philip.g.mak@gmail.com>
2021-04-01 23:18:15 -0400

    Time: 26 ms (26.55%), Space: 40 MB (16.04%) - LeetHub

--- design-a-leaderboard/design-a-leaderboard.java
+++ design-a-leaderboard/design-a-leaderboard.java
@@ -1,63 +1,28 @@
 class Leaderboard {
-    
-    Map<Integer, Pair<Integer, Integer>> playMap;
-    PriorityQueue<Pair<Integer, Integer>> pq;
+    Map<Integer, Integer> map;
+
     public Leaderboard() {
-        this.playMap = new HashMap();
-        this.pq = new PriorityQueue<Pair<Integer, Integer>>((a,b) -> b.getValue() - a.getValue());
-        
+        map = new HashMap<>();
     }
     
     public void addScore(int playerId, int score) {
-        if(this.playMap.containsKey(playerId)){
-            Pair<Integer, Integer> oldScore = this.playMap.get(playerId);
-            Pair<Integer, Integer> newScore = new Pair(playerId, (oldScore.getValue() + score));
-            
-            this.playMap.put(playerId, newScore);
-            this.pq.remove(oldScore);
-            this.pq.add(newScore);
-        } else {
-            this.playMap.put(playerId, new Pair(playerId, score));
-            this.pq.add(new Pair(playerId, score));
-        }
+        map.put(playerId, map.getOrDefault(playerId, 0) + score);
     }
     
     public int top(int K) {
-        List<Pair<Integer, Integer>> temp = new ArrayList();
-        int sum = 0;
-        for(int i = 0; i < K; i++){
-            Pair<Integer, Integer> topScore = pq.poll();
-            
-            sum += topScore.getValue();
-            
-            temp.add(topScore);
-        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
         
-        for(int i = 0; i < temp.size(); i++){
-            
-            pq.add(temp.get(i));
+        int res = 0;
+        while (K > 0 && !pq.isEmpty()) {
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            res += entry.getValue();
+            K--;
         }
-        
-        return sum;
+        return res;
     }
     
     public void reset(int playerId) {
-
-        int negativeScore = this.playMap.get(playerId).getValue() * -1;
-        addScore(playerId, negativeScore);
+        map.put(playerId, 0);
     }
 }
-
-/**
- * Your Leaderboard object will be instantiated and called as such:
- * Leaderboard obj = new Leaderboard();
- * obj.addScore(playerId,score);
- * int param_2 = obj.top(K);
- * obj.reset(playerId);
- */
-
-/*
-idea use hashmap and priorityqueue
-
-HashMap will be <Player ID, Integer object>>
-*/
